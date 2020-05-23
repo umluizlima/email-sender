173d55ad32a28623d202834ec63449de3347db27
Luiz Lima <umluizlima@gmail.com>
2020-05-22 22:04:01 -0300

    Add security module

---
+++ tests/unit/test_security.py
@@ -0,0 +1,21 @@
+from fastapi import HTTPException
+from pytest import fixture, raises
+
+from app.api.security import ApiKeyChecker
+from app.settings import Settings
+
+settings = Settings(API_KEY="some-secret-key")
+
+
+@fixture
+def checker():
+    return ApiKeyChecker(settings=settings)
+
+
+def test_checker_throws_exception_for_invalid_key(checker):
+    with raises(HTTPException):
+        checker(f"not-quite-{settings.API_KEY}")
+
+
+def test_checker_passes_for_valid_key(checker):
+    checker(settings.API_KEY)
