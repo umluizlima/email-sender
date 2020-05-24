95472d57539fb6dd0849140957917e3c10edd1eb
Luiz Lima <umluizlima@gmail.com>
2020-05-24 18:28:35 -0300

    Update adapter usage on router

--- tests/unit/routers/conftest.py
+++ tests/unit/routers/conftest.py
@@ -1,18 +1,34 @@
+from unittest.mock import MagicMock
+
 from pytest import fixture
 from starlette.testclient import TestClient
 
 from app.api import api
 from app.api.security import ApiKeyChecker, api_key_checker
-from app.settings import Settings
+from app.core.adapters import ActiveAdapter, get_active_adapter
+from app.settings import EmailService, Settings
+
+mock_adapter = MagicMock()
+
+
+def get_active_adapter_override():
+    return mock_adapter
+
+
+@fixture
+def adapter():
+    return mock_adapter
 
 
 @fixture
 def settings():
-    return Settings(API_KEY="secret-api-key")
+    return Settings(_env_file=None, API_KEY="secret-api-key")
 
 
 @fixture
 def client(settings):
     client = TestClient(api)
     api.dependency_overrides[api_key_checker] = ApiKeyChecker(settings)
-    return client
+    api.dependency_overrides[get_active_adapter] = get_active_adapter_override
+    yield client
+    api.dependency_overrides = {}
