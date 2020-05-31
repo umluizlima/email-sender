efc7595efd16a236107e01d9e10beff7f8c8077a
Luiz Lima <umluizlima@gmail.com>
2020-05-30 22:13:28 -0300

    Update environment check for https extension

--- app/api/extensions/https.py
+++ app/api/extensions/https.py
@@ -1,6 +1,8 @@
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
+from app.settings import Environment
+
 
 def configure(app, settings):
-    if settings.ENV != "dev":
+    if settings.ENV != Environment.DEVELOPMENT:
         app.add_middleware(HTTPSRedirectMiddleware)
