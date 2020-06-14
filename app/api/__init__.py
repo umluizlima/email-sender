14a5ba7557dce29d9dc1fe42089f1d0bf4c591ce
Luiz Lima <umluizlima@gmail.com>
2020-06-13 21:26:24 -0300

    Add title

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from ..settings import settings
 from . import extensions, routers
 
-api = FastAPI()
+api = FastAPI(title="email-sender")
 
 extensions.configure(api, settings)
 routers.configure(api, settings)
