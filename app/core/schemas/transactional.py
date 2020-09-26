109de5cee1cc5b6457a8c86129191e1697360c51
umluizlima <umluizlima@gmail.com>
2020-09-25 22:45:45 -0300

    Add TransactionalType enum

--- app/core/schemas/transactional.py
+++ app/core/schemas/transactional.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Dict
 
 from pydantic import Field
@@ -5,6 +6,16 @@ from pydantic import Field
 from .email import BaseEmailSchema
 
 
+class TransactionalType(Enum):
+    ACCESS_CODE = ("ACCESS_CODE", "access_code.html")
+
+    def __new__(cls, value: str, template: str):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.template = template
+        return obj
+
+
 class TransactionalSchema(BaseEmailSchema):
-    transactional_type: str = Field(alias="type")
+    transactional_type: TransactionalType = Field(alias="type")
     transactional_data: Dict = Field({}, alias="data")
