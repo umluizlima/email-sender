232aafcd4f3476cc7ba6acaee0010793df67ce2b
umluizlima <umluizlima@gmail.com>
2020-09-27 15:27:16 -0300

    Add transactionals router

--- app/api/routers/transactionals.py
+++ app/api/routers/transactionals.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from starlette.status import HTTP_202_ACCEPTED
 
-from app.core.schemas import EmailSchema
+from app.core.schemas import TransactionalSchema
 from app.core.tasks import Task
 
 from ..dependencies import api_key_checker, tasks_producer
@@ -9,15 +9,15 @@ from ..dependencies import api_key_checker, tasks_producer
 router = APIRouter()
 
 
-@router.post("/emails", status_code=HTTP_202_ACCEPTED)
-def send_email(message: EmailSchema, producer=Depends(tasks_producer)):
-    producer.send_task(Task.SEND_EMAIL, args=[message.dict(by_alias=True)])
+@router.post("/transactionals", status_code=HTTP_202_ACCEPTED)
+def send_transactional(message: TransactionalSchema, producer=Depends(tasks_producer)):
+    producer.send_task(Task.SEND_TRANSACTIONAL, args=[message.dict(by_alias=True)])
 
 
 def configure(app, settings):
     app.include_router(
         router,
-        tags=["emails"],
+        tags=["transactionals"],
         prefix="/api/v1",
         dependencies=[Depends(api_key_checker)],
     )
